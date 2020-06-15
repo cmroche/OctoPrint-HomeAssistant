41b1ebbc8e6bf2d6676850149d0e5152f27d52f5
Clifford Roche <clifford.roche@gmail.com>
2020-06-15 11:30:38 -0400

    Fix typos in plugin URL

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ plugin_author = "Clifford Roche"
 plugin_author_email = "clifford.roche@gmail.com"
 
 # The plugin's homepage URL. Can be overwritten within OctoPrint's internal data via __plugin_url__ in the plugin module
-plugin_url = "https://github.com/cmroche/OctoPrint-HomeAsisstant"
+plugin_url = "https://github.com/cmroche/OctoPrint-HomeAssistant"
 
 # The plugin's license. Can be overwritten within OctoPrint's internal data via __plugin_license__ in the plugin module
 plugin_license = "AGPLv3"
