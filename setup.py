049337cc04b0241349cf2e8155df12badf6431d3
Clifford Roche <clifford.roche@gmail.com>
2020-06-23 11:21:15 -0400

    Prepare release of 2.0.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ plugin_package = "octoprint_homeassistant"
 plugin_name = "OctoPrint-HomeAsisstant"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "1.1.4"
+plugin_version = "2.0.0"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
